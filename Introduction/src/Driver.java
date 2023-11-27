1c997092f26c83543448fbd53b12de4abc282c9b
akbar <akbarshaik9111@gmail.com>
2023-11-27 10:49:52 +0530

    Driver Methods

--- Introduction/src/Driver.java
+++ Introduction/src/Driver.java
@@ -3,8 +3,26 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class Driver {
 	public static void main(String[] args) {
+		//PROVIDED THE LOCATION OF EDGE DRIVER
 		System.setProperty("webdriver.edge.driver", "E:\\Selenium\\msedgedriver.exe");
+		
+		//EDGE DRIVER INVOKED
 		WebDriver driver = new EdgeDriver();
+		
+		driver.close();
+		driver.equals("");
+		//driver.findElement(null);
+		//driver.findElements(null)
+		driver.get(null);
+		driver.getCurrentUrl();
+		driver.getPageSource();
+		driver.getTitle();
+		driver.getWindowHandle();
+		driver.getWindowHandles();
+		driver.manage();
+		//driver.navigate()
+		driver.quit();
+		//driver.switchTo()
 	}
 
 }
